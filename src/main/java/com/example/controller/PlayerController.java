688d6742a7cc63b0d5c93edaf980ad7434800560
Pau111111 <paugarciapellicer@gmail.com>
2016-11-03 19:29:15 +0100

    Controllers actualizados Player & Team

--- src/main/java/com/example/controller/PlayerController.java
+++ src/main/java/com/example/controller/PlayerController.java
@@ -14,34 +14,68 @@ import java.util.List;
 @RestController
 @RequestMapping ("/jugadores")
 public class PlayerController {
-
     @Autowired
     private JugadorRepository jugadorRepository;
 
-    @RequestMapping(method = RequestMethod.POST)
+    @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Jugador createJugador(@RequestBody Jugador jugador) {
         return jugadorRepository.save(jugador);
     }
 
-    @RequestMapping(method = RequestMethod.GET)
-    //find es algo que no puc tocar pero All puc canviar-ho per qualsvol paraula
+    @PutMapping
+    public Jugador updateJugador(@RequestBody Jugador jugador) {
+        return jugadorRepository.save(jugador);
+    }
+
+    @GetMapping
     public List<Jugador> findAll() {
         return jugadorRepository.findAll();
     }
 
-    @RequestMapping(value = "/{id}",
-            method = RequestMethod.GET)
+    @GetMapping("/{id}")
     public Jugador findById(@PathVariable Long id) {
         Jugador jugador = jugadorRepository.findOne(id);
         return jugador;
     }
 
-    @RequestMapping(value = "/byCanastas/{num}",
-            method = RequestMethod.GET)
-    public List<Jugador> findByCanastasGreaterThan(@PathVariable Integer num) {
-        return jugadorRepository.findByCanastasGreaterThanEqual(num);
+    @GetMapping("/byPoints/{num}")
+   public List<Jugador> findByCanastasGreaterThan(@PathVariable Integer num) {
+       return jugadorRepository.findByCanastasGreaterThanEqual(num);
     }
+
+    @DeleteMapping("/{id}")
+    public void deleteJugador(@PathVariable Long id) {
+        jugadorRepository.delete(id);
+    }
+
+//    @Autowired
+//    private JugadorRepository jugadorRepository;
+//
+//    @RequestMapping(method = RequestMethod.POST)
+//    @ResponseStatus(HttpStatus.CREATED)
+//    public Jugador createJugador(@RequestBody Jugador jugador) {
+//        return jugadorRepository.save(jugador);
+//    }
+//
+//    @RequestMapping(method = RequestMethod.GET)
+//    //find es algo que no puc tocar pero All puc canviar-ho per qualsvol paraula
+//    public List<Jugador> findAll() {
+//        return jugadorRepository.findAll();
+//    }
+//
+//    @RequestMapping(value = "/{id}",
+//            method = RequestMethod.GET)
+//    public Jugador findById(@PathVariable Long id) {
+//        Jugador jugador = jugadorRepository.findOne(id);
+//        return jugador;
+//    }
+//
+//    @RequestMapping(value = "/byCanastas/{num}",
+//            method = RequestMethod.GET)
+//    public List<Jugador> findByCanastasGreaterThan(@PathVariable Integer num) {
+//        return jugadorRepository.findByCanastasGreaterThanEqual(num);
+//    }
 }
 
 
